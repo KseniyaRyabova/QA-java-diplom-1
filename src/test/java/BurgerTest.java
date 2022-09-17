8e9fbebb1278f2cc285de78faf3801c10929245a
Kseniya <ryabova.ksyusha@gmail.com>
2022-09-17 20:56:20 +0300

    added tests in BurgerTest

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -7,9 +8,10 @@ import praktikum.Burger;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
-import java.util.Locale;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,7 +29,7 @@ public class BurgerTest {
     Burger burger;
 
     @Test
-    public void getBurgerPriceWithOneIngredient() {
+    public void getBurgerPriceWithOneIngredientTest() {
         when(bun.getPrice()).thenReturn(1F);
         when(ingredient.getPrice()).thenReturn(2F);
 
@@ -42,7 +44,7 @@ public class BurgerTest {
     }
 
     @Test
-    public void getReceiptForBurger() {
+    public void getReceiptForBurgerTest() {
         when(bun.getName()).thenReturn("Вкусняшная пшеничная булочка");
         when(ingredient.getType()).thenReturn(IngredientType.FILLING);
         when(ingredient.getName()).thenReturn("Котлетка");
@@ -66,6 +68,35 @@ public class BurgerTest {
                 bunName, ingredientType, ingredientName, bunName, burgerPrice);
 
         assertEquals("Бургер не тот", expected, actual);
+    }
+
+    @Test
+    public void removeIngredientTest() {
+        Burger burger = new Burger();
+        burger.ingredients = new ArrayList<>(Arrays.asList(ingredient, ingredient));
+        burger.removeIngredient(1);
+
+        int expected = 1;
+        int actual = burger.ingredients.size();
+
+        assertEquals("Ингредиент не был удален", expected, actual);
+    }
+
+    @Test
+    public void moveIngredientTest() {
+        Burger burger = new Burger();
+        burger.ingredients = new ArrayList<>(Arrays.asList(mock(Ingredient.class), mock(Ingredient.class)));
+
+        List<Ingredient> beforeMove = new ArrayList<>(burger.ingredients);
+        burger.moveIngredient(1, 0);
+        List<Ingredient> afterMove = burger.ingredients;
+
+        Iterator<Ingredient> beforeMoveIterator = beforeMove.listIterator();
+        Iterator<Ingredient> afterMoveIterator = afterMove.listIterator();
+
+        while (beforeMoveIterator.hasNext()) {
+            Assert.assertNotEquals(afterMoveIterator.next(), beforeMoveIterator.next());
+        }
     }
 
 }
